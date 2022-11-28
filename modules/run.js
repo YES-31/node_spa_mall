82ce0c678bebf4d7f711de2209d0d9fb9fd7d923
yesk31 <yesk31@naver.com>
2022-11-28 09:10:47 +0900

    First Commit All files

---
+++ modules/run.js
@@ -0,0 +1,6 @@
+
+
+const add = require("./math.js");
+
+
+console.log(add(10, 30));
---
+++ schemas/cart.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema({
+  goodsId: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+
+  quantity : {
+    type : Number,
+    required : true,
+  }
+ 
+});
+
+module.exports = mongoose.model("Cart", cartSchema);
